64aa8c64b8d108050178cdeeb2c1390361e1c15a
max-dmytryshyn <maksumus22@gmail.com>
2021-10-24 19:11:20 +0300

    Add footer

--- Lab 6+/spa/src/components/homepage/footer/Footer.jsx
+++ Lab 6+/spa/src/components/homepage/footer/Footer.jsx
@@ -1,5 +1,39 @@
 import "./Footer.css";
+import facebookIcon from "./images/facebook_icon.png";
+import googleIcon from "./images/google_icon.png";
+import linkedinIcon from "./images/linkedin_icon.png";
+import twitterIcon from "./images/twitter_icon.png";
 
 export const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__upper_part">
+        <div className="branding">
+          <h3 className="branding__header">Saw shop</h3>
+          <p className="branding__paragraph">By the best saws on our site!</p>
+        </div>
+        <div className="footer__logo">
+          <img src="./images/logo.svg" alt="logo" className="footer__logo_img" />
+        </div>
+        <div className="social_media">
+          <a href="">
+            <img src={facebookIcon} alt="social media icon" className="social_media__icon" />
+          </a>
+          <a href="">
+            <img src={twitterIcon} alt="social media icon" className="social_media__icon" />
+          </a>
+          <a href="">
+            <img src={linkedinIcon} alt="social media icon" className="social_media__icon" />
+          </a>
+          <a href="">
+            <img src={googleIcon} alt="social media icon" className="social_media__icon" />
+          </a>
+        </div>
+      </div>
+      <hr className="footer__diving_line" />
+      <div className="footer__lower_part">
+        <p className="lower_part__paragraph">2020 IoT Ⓒ Copyright all rights reserved</p>
+      </div>
+    </footer>
+  );
 };
--- Lab 6+/spa/src/components/homepage/header/Header.jsx
+++ Lab 6+/spa/src/components/homepage/header/Header.jsx
@@ -1,10 +1,9 @@
-import logoImage from "./images/logo.svg";
 import "./Header.css";
 
 export const Header = () => {
   return (
     <header className="header">
-      <img src={logoImage} className="header__logo" alt="logo" />
+      <img src="./images/logo.svg" className="header__logo" alt="logo" />
       <ul className="header__nav">
         <li className="header__nav__item">
           <a href="">Home</a>
