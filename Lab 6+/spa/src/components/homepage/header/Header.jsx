5bc2f19b02530e133b313fef113a990fe4f5fbda
max-dmytryshyn <maksumus22@gmail.com>
2021-10-24 14:20:34 +0300

    Add header

--- Lab 6+/spa/src/components/homepage/header/Header.jsx
+++ Lab 6+/spa/src/components/homepage/header/Header.jsx
@@ -1,5 +1,21 @@
+import logoImage from "./images/logo.svg";
 import "./Header.css";
 
 export const Header = () => {
-  return <header></header>;
+  return (
+    <header className="header">
+      <img src={logoImage} className="header__logo" alt="logo" />
+      <ul className="header__nav">
+        <li className="header__nav__item">
+          <a href="">Home</a>
+        </li>
+        <li className="header__nav__item">
+          <a href="">Catalog</a>
+        </li>
+        <li className="header__nav__item">
+          <a href="">Cart</a>
+        </li>
+      </ul>
+    </header>
+  );
 };
