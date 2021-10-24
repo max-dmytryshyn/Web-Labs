7dd0e0d4348d54b05d6073b43a762bf6ca06ebc5
max-dmytryshyn <maksumus22@gmail.com>
2021-10-24 14:20:26 +0300

    Add general style properties

--- Lab 6+/spa/src/App.jsx
+++ Lab 6+/spa/src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/homepage/header/Header";
 import { Body } from "./components/homepage/body/Body";
 import { Footer } from "./components/homepage/footer/Footer";
+import "./App.css";
 
 function App() {
   return (
