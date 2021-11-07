a8f76fd7e7e6a0da9675d2f64dfc72f0b3933677
max-dmytryshyn <maksumus22@gmail.com>
2021-11-07 13:08:44 +0200

    Add homepage component

---
+++ Lab 6+/spa/src/components/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import { Header } from "../header/Header";
+import { Main } from "./main/Main";
+import { Footer } from "../footer/Footer";
+
+export const Homepage = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
