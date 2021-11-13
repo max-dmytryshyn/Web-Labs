b4be83352ba1b4fb74df2026479db4aaababd0b0
max-dmytryshyn <maksumus22@gmail.com>
2021-11-13 22:20:29 +0200

    Add filtering by length, drive type and saw material to backend

--- Lab 6+/backend/lab_9_backend/saws/models.py
+++ Lab 6+/backend/lab_9_backend/saws/models.py
@@ -1,5 +1,5 @@
 from django.utils.translation import gettext_lazy
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
--- Lab 6+/backend/lab_9_backend/saws/views.py
+++ Lab 6+/backend/lab_9_backend/saws/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 
 from .models import Saw
 from .serializers import SawSerializer
@@ -8,3 +11,23 @@ class SawViewSet(viewsets.ModelViewSet):
 
     queryset = Saw.objects.all()
     serializer_class = SawSerializer
+
+    def get_queryset(self):
+        queryset = Saw.objects.all()
+        min_length = self.request.query_params.get('min_length')
+        max_length = self.request.query_params.get('max_length')
+        drive_type = self.request.query_params.get('drive_type')
+        material_to_saw = self.request.query_params.get('material_to_saw')
+
+        if drive_type:
+            queryset = queryset.filter(drive_type = drive_type)
+
+        if material_to_saw:
+            queryset = queryset.filter(material_to_saw = material_to_saw)
+
+        if min_length:
+            queryset = queryset.filter(length__gt = min_length)
+
+        if max_length:
+            queryset = queryset.filter(lebth__lte = max_length)
+        return queryset
