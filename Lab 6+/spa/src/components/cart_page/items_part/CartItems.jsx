7d010c63eba19538f4dfb2bd8ef160a98b16aa86
max-dmytryshyn <maksumus22@gmail.com>
2021-11-27 17:45:45 +0200

    Add no items text

--- Lab 6+/spa/src/components/cart_page/items_part/CartItems.jsx
+++ Lab 6+/spa/src/components/cart_page/items_part/CartItems.jsx
@@ -12,17 +12,23 @@ export const CartItems = () => {
   ));
 
   return (
-    <div className="cart_items">
-      <div className="cart_items__items">{itemsList}</div>
-      <div className="cart_items__total_price">
-        <p className="cart_items__total_price__label">Total price:</p>
-        <p className="cart_items__total_price__value">
-          $
-          {items.reduce((cur_sum, next_item) => {
-            return cur_sum + next_item.price * next_item.amount;
-          }, 0)}
-        </p>
-      </div>
-    </div>
+    <>
+      {items.length === 0 ? (
+        <h1 className="cart_no_items_text">There are no items in your cart</h1>
+      ) : (
+        <div className="cart_items">
+          <div className="cart_items__items">{itemsList}</div>
+          <div className="cart_items__total_price">
+            <p className="cart_items__total_price__label">Total price:</p>
+            <p className="cart_items__total_price__value">
+              $
+              {items.reduce((cur_sum, next_item) => {
+                return cur_sum + next_item.price * next_item.amount;
+              }, 0)}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
