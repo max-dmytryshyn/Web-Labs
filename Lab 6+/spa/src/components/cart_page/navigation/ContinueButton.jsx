9bb57e3b9624369801a3db272f7f9f38c70bb755
max-dmytryshyn <maksumus22@gmail.com>
2021-12-08 22:46:42 +0200

    Add not ability to create order with no items in the cart

--- Lab 6+/spa/src/components/cart_page/navigation/ContinueButton.jsx
+++ Lab 6+/spa/src/components/cart_page/navigation/ContinueButton.jsx
@@ -1,11 +1,19 @@
 import "./ContinueButton.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import { CartItemsSelector } from "../../../redux/selectors/cartSelectors";
 
 export const ContinueButton = () => {
+  const items = CartItemsSelector();
   return (
-    <Link className="cart_page__continue_button" to="/order">
-      Continue
-    </Link>
+    <>
+      {items.length !== 0 ? (
+        <Link className="cart_page__continue_button" to="/order">
+          Continue
+        </Link>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
