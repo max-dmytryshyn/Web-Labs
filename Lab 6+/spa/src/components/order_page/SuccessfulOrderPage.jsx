2c66498aac61b730d8e195f19f20bc7467a7e9c1
max-dmytryshyn <maksumus22@gmail.com>
2021-12-08 14:53:44 +0200

    Add content to successful order page

--- Lab 6+/spa/src/components/order_page/SuccessfulOrderPage.jsx
+++ Lab 6+/spa/src/components/order_page/SuccessfulOrderPage.jsx
@@ -1,10 +1,25 @@
+import "./SuccessfulOrderPage.css";
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
+import { BackToCatalogButton } from "../cart_page/navigation/BackToCatalogButton";
 
 export const SuccessfulOrderPage = () => {
   return (
     <>
       <Header />
+      <main className="successful_order_main">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/3/3b/Eo_circle_green_checkmark.svg"
+          alt="Success!"
+          className="successful_order_main__image"
+        />
+        <div className="successful_order_main__text">
+          <h1>Success!</h1>
+          <p className="successful_order_main__text__paragraph">Yout order sent to processing!</p>
+          <p className="successful_order_main__text__paragraph">Check your email box for further information</p>
+        </div>
+        <BackToCatalogButton />
+      </main>
       <Footer />
     </>
   );
